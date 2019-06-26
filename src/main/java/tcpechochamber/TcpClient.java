6e8dbc723bb0c7247de62ccc982e19dede315998
SCrawford81299 <thestevencrawford@gmail.com>
2019-06-26 11:15:53 -0600

    temp commit

--- src/main/java/tcpechochamber/TcpClient.java
+++ src/main/java/tcpechochamber/TcpClient.java
@@ -1,9 +1,29 @@
 package tcpechochamber;
 
+import java.io.IOException;
+import java.net.Socket;
+
 public class TcpClient {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+
+		//
 		
+		Socket socket = new Socket("localhost", 27653);
+		byte[] helloBuffer = new byte[200];
+		socket.getInputStream().read(helloBuffer);
+		String helloString = new String(helloBuffer).trim();
+		System.out.println("Recieved from server '" + helloString + "'");
+		if (!helloString.equals("hello")) {
+			throw new RuntimeException("bad Hello");
+		}
+		socket.getOutputStream().write(String.format("%200s", "date").getBytes());
+		byte[] dateResponseBuffer = new byte[200];
+		socket.getInputStream().read(dateResponseBuffer);
+		String rawDateTime = new String(dateResponseBuffer).trim();
+		long dateTime = Long.parseLong(rawDateTime);
+		socket.close();
+
 	}
-	
+
 }
