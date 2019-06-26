4b9c4e724738c90e5194b40e1fc45b3141b02c28
Matthew Maag <MMaag@student.neumont.edu>
2019-06-26 11:14:24 -0600

    Took the stuff from the thing we did in class

--- src/main/java/tcpechochamber/TcpClient.java
+++ src/main/java/tcpechochamber/TcpClient.java
@@ -1,5 +1,44 @@
 package tcpechochamber;
 
+import java.io.IOException;
+import java.net.Socket;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+
 public class TcpClient {
 
+	public static void main(String[] args) throws IOException {
+		Socket socket = new Socket("localhost", 27653);
+		byte[] helloBuffer = new byte[200];
+		
+		socket.getInputStream().read(helloBuffer);
+		String helloString = new String(helloBuffer).trim();
+		System.out.println("Recieved from server '" + helloString + "'");
+		
+		if(!helloString.equals("hello")) {
+			throw new RuntimeException("bad hello");
+		}
+		
+		socket.getOutputStream().write(String.format("%200s", "date").getBytes());
+		
+		byte[] dateResponseBuffer = new byte[200];
+		socket.getInputStream().read(dateResponseBuffer);
+		String rawDateTime = new String(dateResponseBuffer).trim();
+		
+		long dateTime = Long.parseLong(rawDateTime);
+		LocalDateTime displayableTime = LocalDateTime.ofInstant(Instant.ofEpochMilli(dateTime), ZoneId.systemDefault());
+		
+		System.out.println("Server time is '" + displayableTime + "'");
+		socket.close();
+		
+		// Send Size: 200 Bytes
+
+		// Server: Talks first and says "Hello"
+		// Client: What's the DateTime
+		// Server: Sup - Time and Date
+		// Close:
+
+	}
+	
 }
--- src/main/java/tcpechochamber/TcpServer.java
+++ src/main/java/tcpechochamber/TcpServer.java
@@ -1,9 +1,41 @@
 package tcpechochamber;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class TcpServer {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+
+		ServerSocket serverSocket = new ServerSocket(27653);
+
+		Socket socket = serverSocket.accept();
+
+		byte[] hello = String.format("%200s", "hello").getBytes();
+		socket.getOutputStream().write(hello);
+
+		byte[] question = new byte[200];
+		socket.getInputStream().read(question);
+
+		String questionString = new String(question).trim();
 		
+		if (!questionString.contains("date")) {
+			throw new RuntimeException("Bad request");
+		}
+		
+		long currentDateTime = System.currentTimeMillis();
+		byte[] dateResponse = String.format("%200d", currentDateTime).getBytes();
+		
+		socket.getOutputStream().write(dateResponse);
+		socket.close();
+
+		// Send Size: 200 Bytes
+
+		// Server: Talks first and says "Hello"
+		// Client: What's the DateTime
+		// Server: Sup - Time and Date
+		// Close:
 	}
 	
 }
